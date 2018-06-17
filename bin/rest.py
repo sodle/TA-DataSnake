7e8ba3e862646ca0df2203a3b38790427e37f6dd
Scott Odle <sjodle@asu.edu>
2018-06-17 09:41:05 -0700

    hook version-view to version-get

--- bin/rest.py
+++ bin/rest.py
@@ -8,9 +8,6 @@ class VersionCheck(BaseRestHandler):
         BaseRestHandler.__init__(self, method, request_info, response_info, session_key)
         self.response.setHeader('content-type', 'application/json')
 
-    def handle_VIEW(self):
-        self.handle_GET()
-
     def handle_GET(self):
         try:
             datasnake_proc = subprocess.Popen(['datasnake', '--env'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -18,3 +15,5 @@ class VersionCheck(BaseRestHandler):
             self.response.write(out)
         except OSError as e:
             self.response.write(json.dumps({'DataSnake Core': 'Not Installed', 'ex': e.strerror}))
+
+    handle_VIEW = handle_GET
