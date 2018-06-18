f5f709fc4c9e95d09877800e2d830cb0b43744d5
Scott Odle <sjodle@asu.edu>
2018-06-18 15:01:52 -0700

    ouroboros search command using literal connection string (adding cred storage later)

--- bin/ouroboros.py
+++ bin/ouroboros.py
@@ -1,5 +1,7 @@
 from splunklib.searchcommands import Configuration, GeneratingCommand, Option, dispatch
 import sys
+import json
+import subprocess
 
 
 @Configuration(type='reporting')
@@ -8,7 +10,16 @@ class Ouroboros(GeneratingCommand):
     query = Option(require=True)
 
     def generate(self):
-        yield {'success': True}
+        self.logger.error(['datasnake', self.connection, self.query, '--output-format=json'])
+        ds_proc = subprocess.Popen(['datasnake', self.connection, self.query,
+                                    '--output-format=json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ds_out, ds_err = ds_proc.communicate()
+        for line in ds_out.split('\n'):
+            if line.startswith('ROW'):
+                _, _, db_row = line.split('\t')
+                yield json.loads(db_row)
+        for line in ds_err.split('\n'):
+            self.logger.error(line)
 
 
 dispatch(Ouroboros, sys.argv, sys.stdin, sys.stdout, __name__)
