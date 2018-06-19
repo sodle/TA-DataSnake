c6fcd1302105261e76e25ce1f92d98183ec81bad
Scott Odle <sjodle@asu.edu>
2018-06-19 16:15:03 -0700

    add modular input to read a whole table without tailing

---
+++ README/inputs.conf.spec
@@ -0,0 +1,11 @@
+[datasnake_read://<name>]
+* Index the results of a database query in their entirety.
+
+* Connection name as defined in DataSnake setup
+connection = <value>
+
+* SQL Query
+query = <value>
+
+* Format of rows (JSON or classic DBX)
+format = <value>
---
+++ bin/datasnake_read.py
@@ -0,0 +1,38 @@
+from splunklib.modularinput import Script, Scheme, Argument, Event
+import subprocess
+import sys
+from util import ConnectionManager
+
+
+class DataSnakeReadModularInput(Script):
+    def get_scheme(self):
+        scheme = Scheme('DataSnake Reader Modular Input')
+        scheme.description = 'Index the results of a database query in their entirety.'
+        scheme.add_argument(Argument('connection', title='Connection',
+                                     description='Connection name as defined in DataSnake setup',
+                                     data_type=Argument.data_type_string, required_on_create=True))
+        scheme.add_argument(Argument('query', title='Query', data_type=Argument.data_type_string,
+                                     required_on_create=True))
+        scheme.add_argument(Argument('format', title='Format', description='Format of rows (JSON or classic DBX)',
+                                     data_type=Argument.data_type_string,
+                                     validation='format = "dbx" OR format = "json"',  required_on_create=True))
+        return scheme
+
+    def stream_events(self, inputs, ew):
+        for name, item in inputs.inputs.iteritems():
+            conn_man = ConnectionManager(self.service)
+            conn = conn_man.get(item['connection']).clear_password
+            query = item['query']
+            fmt = item['format']
+            ds_proc = subprocess.Popen(['datasnake', conn, query, '--output-format={}'.format(fmt)])
+            ds_out, ds_err = ds_proc.communicate()
+            for line in ds_out.split('\n'):
+                if line.startswith('ROW'):
+                    _, _, row = line.split('\t')
+                    ew.write_event(Event(data=row, stanza=name, source=name, sourcetype='datasnake:{}'.format(fmt)))
+            for line in ds_err.split('\n'):
+                ew.log('ERROR', 'DataSnake error: {}'.format(line))
+
+
+if __name__ == '__main__':
+    sys.exit(DataSnakeReadModularInput().run(sys.argv))
